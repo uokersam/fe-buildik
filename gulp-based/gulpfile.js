40ca6f481a625364a5c2ecd1ab802b06eec676dd
Denis Karakchiev <uokersam@gmail.com>
2019-04-05 18:34:00 +0300

    new gulpfile

--- gulp-based/gulpfile.js
+++ gulp-based/gulpfile.js
@@ -3,89 +3,91 @@ const gulp = require('gulp'),
     browserSync = require('browser-sync'),
     concat = require('gulp-concat'),
     rename = require('gulp-rename'),
-    autoprefixer = require('gulp-autoprefixer'),
+    // autoprefixer = require('gulp-autoprefixer'),
     notify = require('gulp-notify'),
-    csso = require('gulp-csso'),
+    // csso = require('gulp-csso'),
+    // cssbeautify = require('gulp-cssbeautify'),
     twig = require('gulp-twig'),
     data = require('gulp-data'),
-    fs = require('fs');
+    fs = require('fs'),
+    sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', () => {
   browserSync({
-    server: {
-      baseDir: 'public',
-    },
+    server: {baseDir: 'public'},
     notify: false,
-    // open: false,
-    // online: false, // Work Offline Without Internet Connection
-    // tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
   });
 });
 
-gulp.task('twig', function() {
+gulp.task('styles', () => {
+  return gulp.src('app/styles/*.scss').
+      pipe(sourcemaps.init()).
+      pipe(sass({outputStyle: 'expanded'}).
+          on('error', notify.onError())).
+      pipe(rename({suffix: '.min', prefix: ''})).
+      // pipe(autoprefixer(['last 4 versions'])). //todo only prod version
+      // pipe(csso({
+      //   comments: false,
+      // })).
+      // pipe(cssbeautify({
+      //   indent: '  ',
+      //   openbrace: 'separate-line',
+      //   autosemicolon: true,
+      // })).
+      pipe(sourcemaps.write('.')).
+      pipe(gulp.dest('public/css')).
+      pipe(browserSync.stream());
+});
+
+gulp.task('twig', () => {
   return gulp.src('app/**/*.twig').
       pipe(data(function(file) {
-        return JSON.parse(fs.readFileSync('./app/parameters.json'));
+        return JSON.parse(fs.readFileSync('app/options.json'));
       })).
       pipe(twig()).
       pipe(gulp.dest('public'));
 });
 
-gulp.task('styles', function() {
-  return gulp.src('app/sass/**/*.sass').
-      pipe(sass({outputStyle: 'expanded'}).on('error', notify.onError())).
-      pipe(rename({suffix: '.min', prefix: ''})).
-      pipe(autoprefixer(['last 4 versions'])).
-      pipe(csso({
-        comments: false,
-      })).
-      pipe(gulp.dest('public/css')).
-      pipe(browserSync.stream());
-});
-
-gulp.task('scripts', function() {
+gulp.task('scripts', () => {
   return gulp.src([
-    'app/js/common.js', // Always at the end
+    'app/scripts/jquery.min.js',
+    'app/scripts/likely.js',
+    'app/scripts/highlight.pack.js',
+    'app/scripts/buttons.js',
+    'app/scripts/share.js',
+    'app/scripts/common.js', // Always at the end
   ]).
       pipe(concat('scripts.min.js')).
       pipe(gulp.dest('public')).
       pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task('code', function() {
+gulp.task('code', () => {
   return gulp.src('public/**/*.html').
       pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task('img', function() {
-  return gulp.src('app/img/**/*.*').
-      pipe(gulp.dest('public/img'));
+gulp.task('assets', () => {
+  return gulp.src([
+    'app/assets/**/*.*',
+  ]).
+      pipe(gulp.dest('public/assets/'));
 });
 
-gulp.task('fonts', function() {
-  return gulp.src('app/fonts/**/*.*').
-      pipe(gulp.dest('public/fonts'));
-});
-
-gulp.task('files', function() {
-  return gulp.src('app/other/**/*.*').
-      pipe(gulp.dest('public/'));
-});
-
-gulp.task('watch', function() {
-  gulp.watch('app/sass/**/*.sass',
-      gulp.parallel('styles'));
-  gulp.watch(['libs/**/*.js', 'app/js/common.js'], gulp.parallel('scripts'));
+gulp.task('watch', () => {
+  gulp.watch('app/**/*.scss', gulp.parallel('styles'));
+  gulp.watch('app/**/*.js', gulp.parallel('scripts', 'code', 'twig'));
+  gulp.watch(['app/js/common.js'], gulp.parallel('scripts'));
   gulp.watch('app/**/*.twig', gulp.parallel('code', 'twig'));
-  gulp.watch('app/service/**/*.css', gulp.parallel('code', 'twig'));
-  gulp.watch('app/**/*.json', gulp.parallel('code', 'twig'));
+  gulp.watch('app/options.json', gulp.parallel('code', 'twig', 'styles'));
 });
 
 gulp.task('default',
-    gulp.parallel('styles', 'scripts', 'img', 'fonts', 'files', 'twig',
+    gulp.parallel('styles', 'scripts', 'assets', 'twig',
         'browser-sync',
-        'watch'));
+        'watch'),
+);
 
 gulp.task('prod',
-    gulp.parallel('styles', 'scripts', 'img', 'fonts', 'files',
-        'twig'));
+    gulp.parallel('styles', 'scripts', 'assets', 'twig'),
+);
